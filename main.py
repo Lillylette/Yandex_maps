83bbf6b5cf12e544fcd10c8d50d9466df0d6251a
Lillylette <venera4g@gmail.com>
2026-02-16 19:14:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+from PyQt6.QtWidgets import *
+
+
+class MapWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Яндекс Карты")
+        self.setGeometry(100, 100, 800, 600)
+
+        central = QWidget()
+        self.setCentralWidget(central)
+        layout = QVBoxLayout(central)
+
+        input_panel = QHBoxLayout()
+        input_panel.addWidget(QLabel("Долгота:"))
+        self.lon_edit = QLineEdit()
+        input_panel.addWidget(self.lon_edit)
+        input_panel.addWidget(QLabel("Широта:"))
+        self.lat_edit = QLineEdit()
+        input_panel.addWidget(self.lat_edit)
+        input_panel.addWidget(QLabel("Масштаб:"))
+        self.zoom_edit = QLineEdit()
+        input_panel.addWidget(self.zoom_edit)
+        self.load_btn = QPushButton("Загрузить карту")
+        self.load_btn.clicked.connect(self.dummy)
+        input_panel.addWidget(self.load_btn)
+        layout.addLayout(input_panel)
+
+        self.map_label = QLabel()
+        self.map_label.setMinimumSize(600, 400)
+        self.map_label.setStyleSheet("border: 1px solid black;")
+        layout.addWidget(self.map_label)
+
+    def dummy(self):
+        pass
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MapWindow()
+    window.show()
+    sys.exit(app.exec())
